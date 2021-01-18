fcf24c86fe983911c4d40e2ef593a3b2be33c8b7
ckanzafluks <ckanza@fluksaqua.com>
2021-01-18 23:53:56 +0100

    feat: save states

--- src/Security/CustomAuthenticator.php
+++ src/Security/CustomAuthenticator.php
@@ -7,6 +7,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -18,8 +19,11 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Guard\AuthenticatorInterface;
 use Symfony\Component\Security\Guard\PasswordAuthenticatedInterface;
+use Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class CustomAuthenticator extends AbstractFormLoginAuthenticator implements PasswordAuthenticatedInterface
 {
@@ -48,8 +52,7 @@ class CustomAuthenticator extends AbstractFormLoginAuthenticator implements Pass
 
     public function supports(Request $request)
     {
-        return self::LOGIN_ROUTE === $request->attributes->get('_route')
-            && $request->isMethod('POST');
+        return self::LOGIN_ROUTE === $request->attributes->get('_route') && $request->isMethod('POST');
     }
 
     public function getCredentials(Request $request)
@@ -67,28 +70,25 @@ class CustomAuthenticator extends AbstractFormLoginAuthenticator implements Pass
         return $credentials;
     }
 
+    /**
+     * @param mixed $credentials
+     * @param UserProviderInterface $userProvider
+     * @return User|UserInterface|null
+     */
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-
         $user =$this->userRepository->findOneBy(['email' => $credentials['email']]);
         if (!$user) {
-            throw new CustomUserMessageAuthenticationException('Email not found!');// @todo:custom error
+            throw new CustomUserMessageAuthenticationException('Email not found!');
         }
         return $user;
-
-        //return $userProvider->loadUserByUsername($credentials['email']);
-
-        /**
-        $user = $this->entityManager->getRepository(User::class)->findOneBy(['apiToken' => $credentials['apiToken']]);
-        if (!$user) {
-            // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Api Token could not be found.');
-        }
-
-        return $user;
-        **/
     }
 
+    /**
+     * @param mixed $credentials
+     * @param UserInterface $user
+     * @return bool
+     */
     public function checkCredentials($credentials, UserInterface $user)
     {
         return $this->passwordEncoder->isPasswordValid($user, $credentials['password']);
@@ -102,21 +102,47 @@ class CustomAuthenticator extends AbstractFormLoginAuthenticator implements Pass
         return $credentials['password'];
     }
 
+    /**
+     * @param Request $request
+     * @param TokenInterface $token
+     * @param string $providerKey
+     * @return RedirectResponse|null
+     */
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-
-        dump($token);
-        die('success');
-
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
             return new RedirectResponse($targetPath);
         }
         return null;
-
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        //return $this->urlGenerator->generate('user.read');
     }
 
+    /**
+     * @param UserInterface $user
+     * @param string $providerKey
+     * @return \Symfony\Component\Security\Guard\Token\PostAuthenticationGuardToken|void
+     */
+    public function createAuthenticatedToken(UserInterface $user, $providerKey)
+    {
+        return new PostAuthenticationGuardToken(
+            $user,
+            $providerKey,
+            $user->getRoles()
+        );
+    }
+
+    /**
+     * @param Request $request
+     * @param AuthenticationException $exception
+     * @return Response
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        return new Response('Authentication Failure', 401);
+    }
+
+    /**
+     * @return string
+     */
     protected function getLoginUrl()
     {
         return $this->urlGenerator->generate(self::LOGIN_ROUTE);
